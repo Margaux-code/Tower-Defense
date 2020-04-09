78a0a741b8ca2de514f0a06734fd9bcacad3b526
Margaux-code <63307608+Margaux-code@users.noreply.github.com>
2020-04-09 18:09:53 +0200

    Update header.h

--- InfoS2_Octave_Margaux_Clément/header.h
+++ InfoS2_Octave_Margaux_Clément/header.h
@@ -4,13 +4,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <allegro.h>
+#define nb_tour 3
+#define rayon_nuage 50
+#define tire_nuage 3
+#define ralentir_nuage 1
+#define vitesse_missile_nuage 10
+BITMAP *page;
+BITMAP *decor;
+BITMAP *b_tour[nb_tour];
+BITMAP * b_missile[nb_tour];
 
 typedef struct sequence // structure pour recuperer image poney
 {
     char* nomimage;
-    int nimg;        // nombre d'images dans la séquence
-    int tx,ty;       // largeur et hauteur des images de la séquence
-    int ncol;        // nbr images cotes à cotes horizontalement dans le fichier image
+    int nimg;        // nombre d'images dans la sÃ©quence
+    int tx,ty;       // largeur et hauteur des images de la sÃ©quence
+    int ncol;        // nbr images cotes Ã  cotes horizontalement dans le fichier image
     BITMAP* img[80];    // tableau de pointeurs pour indiquer les images
 } t_sequence;
 
@@ -23,5 +32,40 @@ BITMAP * load_bitmap_check(char *nomImage);  // Charger une bitmap
 /// affichage poney ///
 void inimagMech1(t_sequence tab[3]);
 void iniseqMech(t_sequence tab[3]);
+
+
+
+
+typedef enum e_forme e_forme;
+enum e_forme
+{
+    NUAGE=0,ARC_EN_CIEL=1,BONBON=2
+
+};
+
+typedef struct t_missile t_missile;
+struct t_missile
+{
+    int pos_x;
+    int pos_y;
+    int vitesse;
+    int cible_x;
+    int cible_y;
+    int degat;
+};
+
+
+typedef struct t_tour t_tour;
+struct t_tour
+{
+    int pos_x;
+    int pos_y;
+    int rayon_action;
+    int type_tour;
+    int frequence_tir;
+    int ralentir;
+    t_missile missile;
+
+};
 
 #endif // HEADER_H_INCLUDED
