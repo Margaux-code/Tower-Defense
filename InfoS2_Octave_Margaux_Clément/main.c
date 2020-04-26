0a453ab850a182e4a41c9c9f5712dbdeb9121366
Margaux-code <63307608+Margaux-code@users.noreply.github.com>
2020-04-26 17:17:10 +0200

    Update main.c

--- InfoS2_Octave_Margaux_Clément/main.c
+++ InfoS2_Octave_Margaux_Clément/main.c
@@ -8,52 +8,50 @@ int main()
 
     initalisation();
 
-    // Déclaration des pointeurs sur BITMAP devant recevoir des images
+    // DÃ©claration des pointeurs sur BITMAP devant recevoir des images
 
     BITMAP *terrain1;
     BITMAP *TESTterrain1;
     BITMAP *page;
-    page = create_bitmap(800,600);
+    page = create_bitmap(SCREEN_W,SCREEN_H);
+    decor=create_bitmap(SCREEN_W,SCREEN_H);
+    
+    petitsponey=create_bitmap(SCREEN_W,SCREEN_H);
+    regles=create_bitmap(SCREEN_W,SCREEN_H);
+    menu=create_bitmap(SCREEN_W,SCREEN_H);
 
     // Initialisation des poney
 
     t_sequence tab[3]; // Declaration du tableau avec sequence image poney
-    t_poney pon[3]; // Declaration du tableau avec les différentes sorte de poney
+    t_poney pon[3]; // Declaration du tableau avec les diffÃ©rentes sorte de poney
     t_poney acteur[100];
     int nbActeur;
     inimagMech1(tab);
     iniMech(pon,tab);
     nbActeur = creaTabActeur(acteur,pon,nbActeur);
     printf("%d",nbActeur);
-    // Chargement des images (l'allocation a lieu en même temps)
+    // Chargement des images (l'allocation a lieu en mÃªme temps)
     terrain1 = load_bitmap_check ("image/terrain/terrainlvl1.bmp");
     TESTterrain1 = load_bitmap_check ("image/terrain/ligneterrainlvl1.bmp");
 
     int i=0;
     int j;
 
-    while (!key[KEY_ESC])
+petitsponey=load_bitmap("Intro.bmp",NULL);
+    if (!petitsponey)
     {
-        affichageMech(acteur,nbActeur);
-        if (i==4)
-        {
-            i=0;
-        }
-        blit(terrain1, page, 0,0,0,0, terrain1->w, terrain1->h);
-        for(j=0;j<nbActeur;j++)
-        {
-            //allegro_message("yoyo");
-            rectfill(page,j*64+2,j*64-5,j*64+62,j*64,makecol(0,0,0));
-            rectfill(page,j*64+4,j*64-4,j*64+i*15,j*64-1,makecol(0,255,0));
-            draw_sprite(page,acteur[j].seq.img[acteur[j].numImg[i]], j*64, j*64);
-            //allegro_message("yoyo");
-        }
-        blit(page,screen,0,0,0,0,800,600);
-        i++;
-        rest(100);
-        clear_bitmap(page);
-        clear_bitmap(screen);
+        allegro_message("pas pu trouver intro.bmp");
+        exit(EXIT_FAILURE);
     }
+    menu=load_bitmap("menu.bmp",NULL);
+        if (!menu)
+    {
+        allegro_message("pas pu trouver menu.bmp");
+        exit(EXIT_FAILURE);
+    }
+    t_tour * nuage;
+    show_mouse(screen);
+    jeu_presentation();
     return 0;
 
 }
