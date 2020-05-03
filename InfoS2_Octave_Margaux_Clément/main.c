247f9f4044ee6393152597b583aa7aaf906ea01d
Margaux-code <63307608+Margaux-code@users.noreply.github.com>
2020-05-03 20:11:52 +0200

    Update main.c

--- InfoS2_Octave_Margaux_Clément/main.c
+++ InfoS2_Octave_Margaux_Clément/main.c
@@ -12,29 +12,7 @@ int main()
 
     srand(time(NULL));
 
-    // Déclaration des pointeurs sur BITMAP devant recevoir des images
-
-    page = create_bitmap(SCREEN_W,SCREEN_H);
-    petitsponey=create_bitmap(SCREEN_W,SCREEN_H);
-    regles=create_bitmap(SCREEN_W,SCREEN_H);
-    menu=create_bitmap(SCREEN_W,SCREEN_H);
-    buffer=create_bitmap(SCREEN_W,SCREEN_H);
-    b_distributeur=create_bitmap(SCREEN_W,SCREEN_H);
-    b_missile=create_bitmap(SCREEN_W,SCREEN_H);
-
-    petitsponey=load_bitmap("image/Intro.bmp",NULL);
-    if (!petitsponey)
-    {
-        allegro_message("pas pu trouver intro.bmp");
-        exit(EXIT_FAILURE);
-    }
-    menu=load_bitmap("image/Menu.bmp",NULL);
-    if (!menu)
-    {
-        allegro_message("pas pu trouver menu.bmp");
-        exit(EXIT_FAILURE);
-    }
-    b_missile=load_bitmap("image/missiles.bmp",NULL);
+    chargement_bitmap();
     show_mouse(screen);
     jeu_presentation();
     return 0;
